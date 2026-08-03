bb9aac66145f4e22e954251d989341cab3b5aff6
Varoto7 <szymonej9@gmail.com>
2026-08-03 13:53:04 +0200

    Przywrócenie wrangler.toml i naprawa konfiguracji D1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
